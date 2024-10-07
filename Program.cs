aa8f272a7ab660a0c738b1adfcb8161c351e4c97
Ibrahim Akinyele <iakinyele3@gmail.com>
2024-10-07 21:41:49 +0100

    Tasks in Progress

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 1
+
+string name = "John Doe";
+
+int age = 25;
+
+bool isAdmin = true;
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(isAdmin);
+
+
+// Task 2
+
+int num = 12;
+if (num % 2 == 0) {
+    Console.WriteLine("Even");
+} else {
+    Console.WriteLine("Odd");
+}
+
+// Task 3
+for (int i = 1; i <= 10; i++) {
+    Console.WriteLine(i);
+}
+
+// Task 4
+
+
+// Task 5
+
+void Greet(String name) {
+    Console.WriteLine("Hello, {0}!", name);
+}
+
+Greet("Alice");
